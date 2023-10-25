e2982b70a47403938d44073e9ea44eab17f3f7ad
Sanjeev Chauhan <sanjeev.chauhan@duke.edu>
2023-10-25 14:55:15 -0400

    using python approach

---
+++ toL0AFEND/save_struct_to_openPMD.m
@@ -0,0 +1,26 @@
+function convertGDFtoOpenPMD(gdfFilePath)
+    % Define paths and names
+    venvName = 'gdf_to_openpmd';
+    venvPath = fullfile(pwd, venvName);
+    pythonExe = fullfile(venvPath, 'bin', 'python'); % For macOS and Linux
+    % pythonExe = fullfile(venvPath, 'Scripts', 'python.exe'); % Uncomment for Windows
+
+    % Check if virtual environment already exists
+    if ~isfolder(venvPath)
+        % Create a new virtual environment
+        system(sprintf('python -m venv %s', venvName));
+
+        % Activate the virtual environment and install packages
+        system(sprintf('%s -m pip install openpmd-api~=0.13.0,~=0.14.0, matplotlib>=3.0.0', pythonExe));
+
+    end
+
+    % Set MATLAB to use the Python from the virtual environment
+    pyversion(pythonExe);
+
+    % Call the gdf_to_openPMD.py script
+    pythonScriptPath = 'path_to_gdf_to_openPMD.py'; % Update this to the actual path
+    system(sprintf('%s %s %s', pythonExe, pythonScriptPath, gdfFilePath));
+
+    disp('Conversion completed!');
+end
